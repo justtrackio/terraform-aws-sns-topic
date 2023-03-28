ed22098abe6541e4e37fbd6658824345c4169a48
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-03-28 12:41:13 +0200

    feat: add_alarm_description_json

--- variables.tf
+++ variables.tf
@@ -10,20 +10,16 @@ variable "alarm_datapoints_to_alarm" {
   description = "The number of datapoints that must be breaching to trigger the alarm"
 }
 
+variable "alarm_description" {
+  type        = string
+  description = "The description for the alarm"
+  default     = null
+}
+
 variable "alarm_evaluation_periods" {
   type        = number
   default     = 3
   description = "The number of periods over which data is compared to the specified threshold"
-}
-
-variable "alarm_label_order" {
-  type        = list(string)
-  description = <<-EOT
-    The order in which the labels (ID elements) appear in the `id`.
-    Defaults to ["namespace", "environment", "stage", "name", "attributes"].
-    You can omit any of the 6 labels ("tenant" is the 6th), but at least one must be present.
-    EOT
-  default     = ["stage", "name"]
 }
 
 variable "alarm_period" {
@@ -42,6 +38,16 @@ variable "alarm_topic_arn" {
   type        = string
   description = "The ARN of the topic to send alarms to"
   default     = null
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "AWS Account ID"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
 }
 
 variable "principals_with_publish_permission" {
@@ -72,4 +78,12 @@ variable "sqs_success_feedback_sample_rate" {
   type        = number
   description = "Percentage of how many samples to take"
   default     = null
+}
+
+variable "label_orders" {
+  type = object({
+    cloudwatch = optional(list(string))
+  })
+  default     = {}
+  description = "Overrides the `labels_order` for the different labels to modify ID elements appear in the `id`"
 }
